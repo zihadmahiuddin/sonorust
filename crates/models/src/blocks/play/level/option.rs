160ad210576f8c10c71a5765668067e164363219
Zihad <zihadmahiuddin@gmail.com>
2026-07-02 22:31:34 +0600

    feat: add proper construrctor to PlayLevelOption

--- crates/models/src/blocks/play/level/option.rs
+++ crates/models/src/blocks/play/level/option.rs
@@ -1,13 +1,32 @@
 use sonorust_ir::IRValue;
 use tracing::warn;
 
-use crate::blocks::ReadableBlock;
+use crate::{blocks::ReadableBlock, engine::configuration::option::EngineOption};
 
-#[derive(Debug, Default)]
+#[derive(Debug)]
 pub struct PlayLevelOption(pub Vec<IRValue>);
 
 impl PlayLevelOption {
     pub const BLOCK_ID: u64 = 2002;
+
+    pub fn new(options: &[EngineOption]) -> Self {
+        Self(
+            options
+                .iter()
+                .map(|option| match option {
+                    EngineOption::Slider { def, name, .. } => {
+                        if name == "#NOTE_SPEED" {
+                            10.5
+                        } else {
+                            *def
+                        }
+                    }
+                    EngineOption::Toggle { def, .. } => *def,
+                    EngineOption::Select { def, .. } => *def,
+                })
+                .collect(),
+        )
+    }
 }
 
 impl ReadableBlock for PlayLevelOption {
